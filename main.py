5961a4a43ccbedee9a73959a8e83af3d7fa94ba9
Cheburator <sashadanilenko06@gmail.com>
2023-06-23 00:14:04 +0300

    optimised

--- main.py
+++ main.py
@@ -1,65 +1,56 @@
-def func():
+OPER_LIST = ['+', '-', '*', '/', '%']
 
+def operation(num_1, oper, num_2):
+     result = eval(str(num_1) + oper + str(num_2))
+     return result
+
+def main_func():
     try:
         num_1 = float(input('---Enter the first number--> '))
-        def oper():
-            operation = input('---Enter the type of operation[+, -, *, /, %]--> ')
+        def oper(num_1):
+            oper = input(f'---Enter the type of operation{OPER_LIST}--> ')
             try:
                 num_2 = float(input('---Enter the second number--> '))
             except ValueError:
                 print('\n!!!This input has to be a number\n\n')
-                func()
+                main_func()
 
             global answ
-            if operation == '+':
-                    answ = num_1 + num_2
 
-            elif operation == '-':
-                    answ = num_1 - num_2
-
-            elif operation == '*':
-                    answ = num_1*num_2
-
-            elif operation == '/':
-                    answ = num_1/num_2
-                
-            elif operation == '%':
-                    answ = num_1/num_2*100
-
+            if oper in OPER_LIST:
+                if num_2:
+                    answ = operation(num_1, oper, num_2)
             else:
-                 print('\n!!!Incorrect operation\n***Here is the list of operations:[+, -, *, /, %]\n\n')
-                 func()
-            print('\n***Intermediate result is--> ', answ)
-        oper()
+                 print(f'\n!!!Incorrect operation\n***Here is the list of operations:{OPER_LIST}\n\n')
+                 main_func()
+            print(f'\n***Intermediate result is--> {answ}')
+        oper(num_1)
 
 
         while True:
-            one_more_oper = input('\n---One more operation? [y/n]: ') 
+            one_more_oper = input('\n---One more operation? [y/n]: ').lower()
             print('')
             if one_more_oper == 'y':         
-                num_1 = answ
-                oper()
+                oper(answ)
             elif one_more_oper == 'n':
-                print('\n***The final result is--> ', answ)
+                print(f'\n***The final result is--> {answ}')
                 break
             else:
-                print('\n***The final result was--> ', answ)
-                print('\n!!!But you had to choose [y/n]')
-                break
-                
+                print(f'\n***The final result was--> {answ}\n!!!But you had to choose [y/n]')
+                break      
                           
     except ValueError:
         print('\n!!!This input has to be a number\n\n')
-        func()
+        main_func()
 
-func()
+main_func()
 
 
 while True:
-    again_yes_or_no = input('\n\n---Again? [y/n]: ')
+    again_yes_or_no = input('\n\n---Again? [y/n]: ').lower()
 
     if again_yes_or_no == 'y':
         print('\n')
-        func()
+        main_func()
     else:
         quit()
